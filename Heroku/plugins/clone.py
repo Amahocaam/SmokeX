0ea7dd38224e944c04a992cc7aa724b93b359be6
gє¢є кυѕ̧υ <117168172+Amahocaam@users.noreply.github.com>
2022-11-19 20:09:07 +0300

    Update clone.py

--- Heroku/plugins/clone.py
+++ Heroku/plugins/clone.py
@@ -10,7 +10,7 @@ from Heroku.config import API_ID, API_HASH
 IMG = ["https://telegra.ph/file/cefd3211a5acdcd332415.jpg", "https://telegra.ph/file/30d743cea510c563af6e3.jpg", "https://telegra.ph/file/f7ae22a1491f530c05279.jpg", "https://telegra.ph/file/2f1c9c98452ae9a958f7d.jpg"]
 MESSAGE = "Heya! I'm a music bot hoster/Cloner\n\nI can Host Your Bot On My Server within seconds\n\nTry /clone Token from @botfather"
 
-@cloner.on_message(filters.private & filters.command("start"))
+@cloner.on_message(filters.private & filters.command("dkkdej"))
 async def hello(client, message: Message):
     buttons = [
            [
@@ -29,18 +29,18 @@ async def hello(client, message: Message):
 @cloner.on_message(filters.private & filters.command("clone"))
 async def clone(bot, msg: Message):
     chat = msg.chat
-    text = await msg.reply("Usage:\n\n /clone token")
+    text = await msg.reply("Kullanım:\n\n /clone token")
     cmd = msg.command
     phone = msg.command[1]
     try:
-        await text.edit("Booting Your Client")
+        await text.edit("Yükleniyor... ")
                    # change this Directry according to ur repo
         client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "Heroku.modules"})
         await client.start()
         user = await client.get_me()
-        await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @{ASSUSERNAME} To Your Chat!\n\nThanks for Cloning.")
+        await msg.reply(f"Başarıyla Başlatıldı @{user.username}! ✅ \n\n Şimdi Botunuzu ve Asistanı @{ASSUSERNAME} Grubunuza Ekleyin!")
     except Exception as e:
-        await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+        await msg.reply(f"**HATA:** `{str(e)}`\nYeniden /start veriniz.")
 #End
 ##This code fit with every pyrogram Codes just import then @Client Xyz!
 
